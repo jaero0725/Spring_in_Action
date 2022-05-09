368921a794f6923d50e96f825f2b6734bd12bd32
choijaeho <dhqkgbs@naver.com>
2022-05-09 23:31:06 +0900

    chap 02

--- chap01/taco_cloud/src/main/java/tacos/Ingredient.java
+++ chap01/taco_cloud/src/main/java/tacos/Ingredient.java
@@ -1,5 +1,16 @@
 package tacos;
 
-public class Ingredient {
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
+@Data
+@RequiredArgsConstructor
+public class Ingredient {
+	private final String id;
+	private final String name;
+	private final Type type;
+	
+	public static enum Type {
+		WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+	}
 }
